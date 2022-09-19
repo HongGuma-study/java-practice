0f57fad095a3e6c6d34d6358476dbd356a75b4dd
HongGuma <hongsu9849@gmail.com>
2022-09-19 21:27:00 +0900

    practice-1

---
+++ java/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Java Word - 1");
+        System.out.println("Hello Java Word - 2");
+        System.out.println("Hello Java Word - 3");
+        System.out.println("Hello Java Word - 5");
+        System.out.println("Hello Java Word - 6");
+
+        System.out.println("1");
+        System.out.print("2");
+        System.out.printf("3");
+        System.out.println();
+    }
+}
