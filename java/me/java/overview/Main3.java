efde196faf7f247934d20c262f3a0bf8b6cfefde
HongGuma <hongsu9849@gmail.com>
2022-09-19 21:55:08 +0900

    실습 3번에 차이점 주석 추가

--- java/me/java/overview/Main3.java
+++ java/me/java/overview/Main3.java
@@ -3,8 +3,8 @@ package me.java.overview;
 public class Main3 {
     public static void main(String[] args) {
         int a = 10;
-        System.out.println(a);
-        System.out.println('a');
-        System.out.println("a");
+        System.out.println(a);    //변수
+        System.out.println('a');  //문자 : 문자 한개만 가능
+        System.out.println("a");  //문자열 : 문자 여러개 가능
     }
 }
